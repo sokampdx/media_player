487cda4b55e4b1f7b39fd707609b23df5f41e8a5
sokampdx <anthonykpso@gmail.com>
2018-03-26 22:42:50 -0700

    complete simple playback

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,19 +1,23 @@
-package sample;
+package application;
 
 import application.Player;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Player player = new Player("file:///Users/anthonyso/Documents/programs/java_projects/test.mp4");
-
-    }
+  @Override
+  public void start(Stage primaryStage) throws Exception{
+    String file = "file:///Users/" + System.getenv("USER") + "/programs/java_projects/media_player/src/application/test.mp4";
+    Player player = new Player(file);
+    Scene scene = new Scene(player, 1280, 760, Color.BLACK);
+    primaryStage.setScene(scene);
+    primaryStage.show();
+  }
 
 
     public static void main(String[] args) {
--- src/application/Player.java
+++ src/application/Player.java
@@ -1,4 +1,27 @@
 package application;
 
-public class Player {
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+
+public class Player extends BorderPane {
+  Media media;
+  MediaPlayer player;
+  MediaView view;
+  Pane pane;
+
+  public Player(String file) {
+    media = new Media(file);
+    player = new MediaPlayer(media);
+    view = new MediaView(player);
+    pane = new Pane();
+
+    pane.getChildren().add(view);
+
+    setCenter(pane);
+
+    player.play();
+  }
 }
