2d68609718be4e28cc447344ea5859de2ae78391
sokampdx <anthonykpso@gmail.com>
2018-03-28 21:00:51 -0700

    add menu

--- src/application/Main.java
+++ src/application/Main.java
@@ -5,6 +5,9 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
@@ -12,8 +15,16 @@ public class Main extends Application {
 
   @Override
   public void start(Stage primaryStage) throws Exception{
-    String file = "file:///Users/" + System.getenv("USER") + "/programs/java_projects/test.mp4";
-    Player player = new Player(file);
+    MenuItem open = new MenuItem("Open");
+    Menu file = new Menu("File");
+    MenuBar menu = new MenuBar();
+
+    file.getItems().add(open);
+    menu.getMenus().add(file);
+
+    String filename = "file:///Users/" + System.getenv("USER") + "/programs/java_projects/test.mp4";
+    Player player = new Player(filename);
+    player.setTop(menu);
     Scene scene = new Scene(player, 1280, 760, Color.BLACK);
     primaryStage.setScene(scene);
     primaryStage.show();
