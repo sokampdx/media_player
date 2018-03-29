68f0b53bfdd0156da46a4d25b609a1e80594b901
sokampdx <anthonykpso@gmail.com>
2018-03-28 21:30:42 -0700

    add file chooser functionality

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,6 +2,8 @@ package application;
 
 import application.Player;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -9,12 +11,32 @@ import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
 import javafx.scene.paint.Color;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+import java.io.File;
+import java.net.MalformedURLException;
 
-  @Override
-  public void start(Stage primaryStage) throws Exception{
+public class Main extends Application {
+  Player player;
+  FileChooser fileChooser;
+
+  public void start(final Stage primaryStage) throws Exception{
+    String filename = "file:///Users/" + System.getenv("USER") + "/programs/java_projects/test.mp4";
+
+    MenuBar menu = create_menu_bar(primaryStage);
+    Scene scene = create_scene(menu, filename);
+    primaryStage.setScene(scene);
+    primaryStage.show();
+  }
+
+  private Scene create_scene(MenuBar menu, String filename ) {
+    player = new Player(filename);
+    player.setTop(menu);
+    return new Scene(player, 1280, 760, Color.BLACK);
+  }
+
+  private MenuBar create_menu_bar(final Stage primaryStage) {
     MenuItem open = new MenuItem("Open");
     Menu file = new Menu("File");
     MenuBar menu = new MenuBar();
@@ -22,16 +44,32 @@ public class Main extends Application {
     file.getItems().add(open);
     menu.getMenus().add(file);
 
-    String filename = "file:///Users/" + System.getenv("USER") + "/programs/java_projects/test.mp4";
-    Player player = new Player(filename);
-    player.setTop(menu);
-    Scene scene = new Scene(player, 1280, 760, Color.BLACK);
-    primaryStage.setScene(scene);
-    primaryStage.show();
+    fileChooser = new FileChooser();
+    handle_open_file(primaryStage, open, menu);
+    return menu;
+  }
+
+  private void handle_open_file(final Stage primaryStage, MenuItem open, final MenuBar menu) {
+    open.setOnAction(new EventHandler<ActionEvent>() {
+      public void handle(ActionEvent event) {
+        player.player.pause();
+        File file = fileChooser.showOpenDialog(primaryStage);
+        String filename;
+        if (file != null) {
+          try {
+            filename = file.toURI().toURL().toExternalForm();
+            Scene scene = create_scene(menu, filename);
+            primaryStage.setScene(scene);
+          } catch (MalformedURLException e1) {
+            e1.printStackTrace();
+          }
+        }
+      }
+    });
   }
 
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
         launch(args);
     }
 }
